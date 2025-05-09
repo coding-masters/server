30471c21f1c7c6599b0efe7172274717d802a695
mingmingmon <mingmingmon@naver.com>
2025-05-09 18:43:49 +0900

    setting: 스프링 부트 세팅 #1

---
+++ src/main/java/com/example/codingmasters/CodingMastersApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingmasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingMastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingMastersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codingmasters/CodingMastersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingmasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingMastersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
