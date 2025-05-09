78a5835ecf0c068a8239c25dd5e59211a58272f5
mingmingmon <mingmingmon@naver.com>
2025-05-09 19:03:10 +0900

    setting: BaseEntity, 응답 포맷 설정 #1

---
+++ src/main/java/com/example/codingmasters/global/response/GeneralException.java
@@ -0,0 +1,15 @@
+package com.example.codingmasters.global.response;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private Status status;
+
+    public Body getBody() {
+        return this.status.getBody();
+    }
+
+}
+
